787aaf67fdd9321d40cb397f8cb56cbe7561eaf8
RezaualHossenSunny <91420025+RezaualHossenSunny@users.noreply.github.com>
2024-03-31 17:38:42 +0600

    forget password responcive done

--- src/Pages/ForgetPassword/Forgetpass.jsx
+++ src/Pages/ForgetPassword/Forgetpass.jsx
@@ -22,19 +22,19 @@ export const Forgetpass = () => {
   }
 
   return (
-    <div className='h-screen w-full bg-primary flex items-center justify-center'>
-        <div className='bg-white p-20 border border-[5px] rounded'>
-            <h1 className='font-open text-hedding font-bold text-3xl'>Forgot Password</h1>
+    <div className=' md:h-screen w-full bg-primary md:flex items-center justify-center'>
+        <div className='bg-white p-20  border-[5px] rounded'>
+            <h1 className='font-open text-hedding font-bold text-xl md:text-3xl'>Forgot Password</h1>
 
-            <div className='mt-16 relative'>
-            <input onChange={(e)=> setEmail(e.target.value)}  className='py-6  w-96   border-b border-[#BFBFBF] focus:outline-0 ' type="email" placeholder='enter your email email' />
+            <div className=' mt-4 md:mt-16 relative'>
+            <input onChange={(e)=> setEmail(e.target.value)}  className='py-6  w-full md:w-96   border-b border-[#BFBFBF] focus:outline-0 ' type="email" placeholder='enter your email email' />
             <p className='absolute top-[-13px]   font-open font-semibold text-hedding bg-white tracking-wide'>Email Address</p>
         
           </div>
           <div className='flex gap-x-5 mt-10'>
-          <div onClick={handleReset}  className='font-open font-semiBold font-xl bg-[#FF5233] text-white p-5 text-center rounded-[5px] cursor-pointer'>Reset </div>
+          <div onClick={handleReset}  className='font-open font-semiBold text-sm md:font-xl bg-[#FF5233] text-white p-2 md:p-5 text-center rounded-[5px] cursor-pointer'>Reset </div>
 
-          <div className='font-open font-semiBold font-xl bg-[#3498DB] text-white p-5 text-center rounded-[5px] cursor-pointer'> <Link to='/login'>Back To Login  </Link> </div>
+          <div className='font-open font-semiBold text-sm md:font-xl bg-[#3498DB] text-white p-2 md:p-5 text-center rounded-[5px] cursor-pointer'> <Link to='/login'>Back To Login  </Link> </div>
 
           
 
